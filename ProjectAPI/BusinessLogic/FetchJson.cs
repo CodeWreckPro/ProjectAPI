eb2db96db194170a5a2adda155971e30b37080a6
Rakshith S <79991372+CodeWreckPro@users.noreply.github.com>
2023-06-22 02:17:30 +0530

    Update FetchJson.cs
    Anonymised the GET request

--- ProjectAPI/BusinessLogic/FetchJson.cs
+++ ProjectAPI/BusinessLogic/FetchJson.cs
@@ -13,7 +13,7 @@ namespace ProjectAPI.BusinessLogic
         public async Task<ChangeSets> FetchTasksListAsync(int id)
         {
             id = 20230505;
-            var URL = String.Format("https://hera.healthcare.siemens.com/tfs/AXProd/XRay/_apis/pipelines/#{0}?api-version=6.0-preview.1", id);
+            var URL = String.Format("Your Get Request/{0}/URI Parameters?api-version=6.0-preview.1", id);
             var response = await client.GetAsync(URL);
             string responseString = await response.Content.ReadAsStringAsync();
             ChangeSets resJson = JsonSerializer.Deserialize<ChangeSets>(responseString);
